5cee75095b25d72b659fe966cd2233035d63674f
Albykps <albericq.kponso@sicmagroup.com>
2022-05-19 18:35:11 +0100

    all usersControllers 19/05/2022

--- backend/controllers/users.controllers.js
+++ backend/controllers/users.controllers.js
@@ -1,9 +1,15 @@
+const User = require("../models/User")
 module.exports.allUser = async(req,res)=>{
-    console.log(req);
-    res.status(200).json({message:"nothing"});
+    try {
+        let users = await User.find().select("-password");
+        return res.status(200).json({users})
+    } catch (error) {
+        return res.status(500).json({error});   
+    }
+ 
 }
 module.exports.oneUser = async(req,res)=>{
-    console.log(req);
+    
     res.status(200).json({message:"nothing"});
 }
 module.exports.updateUser = async(req,res)=>{
