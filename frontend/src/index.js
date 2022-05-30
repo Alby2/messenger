2d2bb9979eb2658d22fed50a574d5549bd2fec98
Alby2 <alby2000kps@gmail.com>
2022-05-30 13:59:36 +0100

    30/05/2022 front-end

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
+
+ReactDOM.render(
+  
+  <>
+    Hello....!,I'm here
+  </>,
+  document.getElementById('root')
+);
+
