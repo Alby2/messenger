c7c45fbf389e4ca16b3551a37f92efbc41f3b2f6
Alby2 <alby2000kps@gmail.com>
2022-05-31 15:29:08 +0100

    31/05/2022  end friend controller and test

--- backend/controllers/friends.controllers.js
+++ backend/controllers/friends.controllers.js
@@ -22,17 +22,13 @@ module.exports.addFriend = async(req,res)=>{
         return res.status(500).json({err:"Id invalid"})
     }
     try {
-        await User.findByIdAndUpdate(idAdd,{$addToSet:{invite:idConfirm}},{new:true,upsert:true},(err,data)=>{
-            if (err || data == null) {
-                return res.status(400).json({message:'Utilisateur introuvable'})
-            }
+        await User.findByIdAndUpdate(idAdd,{$addToSet:{invit:idConfirm}},{new:true,upsert:true}).catch((err)=>{
+            return res.status(400).json({message:'Utilisateur introuvable'})
         })
-        await User.findByIdAndUpdate(idConfirm,{$addToSet:{confirm:idAdd}},{new:true,upsert:true},(err,data)=>{
-            if (err || data == null) {
-                return res.status(400).json({message:'Utilisateur introuvable'})
-            }else{
-                return res.status(200).json({message:"Utilisateur ajouté avec success"})
-            }
+        await User.findByIdAndUpdate(idConfirm,{$addToSet:{confirm:idAdd}},{new:true,upsert:true}).then((docs)=>{
+            return res.status(200).json({message:'Utilisateur ajouté'})
+        }).catch((err)=>{
+            return res.status(500).json({message:'Utilisateur introuvable'})
         })
     } catch (error) {
         return res.status(500).json({error})
@@ -43,21 +39,17 @@ module.exports.addFriend = async(req,res)=>{
 module.exports.confirmFriend = async(req,res)=>{
     const idAdd = req.params.id;
     const idConfirm = req.body.confirm;  
-    if (!verify.isValid(idAdd) || !verify.isValid(idConfirm)) {
+    if (!verifyId.isValid(idAdd) || !verifyId.isValid(idConfirm)) {
         return res.status(500).json({err:"Id invalid"})
     }
     try {
-        await User.findByIdAndUpdate(idAdd,{$pull:{invite:idConfirm},$addToSet:{friends:idConfirm}},{new:true,upsert:true},(err,data)=>{
-            if (err || data == null) {
+        await User.findByIdAndUpdate(idAdd,{$pull:{invit:idConfirm},$addToSet:{friends:idConfirm}},{new:true,upsert:true}).catch((err)=>{
                 return res.status(400).json({message:'Utilisateur introuvable'})
-            }
         })
-        await User.findByIdAndUpdate(idConfirm,{$pull:{confirm:idAdd},$addToSet:{friends:idAdd}},{new:true,upsert:true},(err,data)=>{
-            if (err || data == null) {
-                return res.status(400).json({message:'Utilisateur introuvable'})
-            }else{
-                return res.status(200).json({message:"Utilisateur confirmé avec succes"})
-            }
+        await User.findByIdAndUpdate(idConfirm,{$pull:{confirm:idAdd},$addToSet:{friends:idAdd}},{new:true,upsert:true}).then((data)=>{
+            return res.status(200).json({message:"Utilisateur confirmé avec succes"})
+        }).catch((e)=>{
+            return res.status(400).json({message:'Utilisateur introuvable'})
         })
     
     } catch (error) {
@@ -69,24 +61,20 @@ module.exports.refuseConfirmFriend = async(req,res)=>{
     //Par default il d'agit de supprimer une invitation donc demande d'amitie
     // inverst = 0 =>'Supprimer l'invitation d'une personne'
     // inverst = 1 => 'Annuler l'invitation qu'on a envoyé à une personne
-    const inverst = req.body
+    const inverst = req.body.medo
     const idMe = (inverst == 1)? req.params.idMe:req.body.idFriend;
     const idFriend = (inverst == 1)? req.body.idFriend:req.params.idMe;  
-    if (!verify.isValid(idMe) || !verify.isValid(idFriend)) {
+    if (!verifyId.isValid(idMe) || !verifyId.isValid(idFriend)) {
         return res.status(500).json({err:"Id invalid"})
     }
     try {
-        await User.findByIdAndUpdate(idMe,{$pull:{invite:idFriend}},{new:true,upsert:true},(err,data)=>{
-            if (err || data == null) {
-                return res.status(400).json({message:'Utilisateur introuvable'})
-            }
+        await User.findByIdAndUpdate(idMe,{$pull:{invit:idFriend}},{new:true,upsert:true}).catch((err)=>{
+            return res.status(400).json({message:'Utilisateur introuvable'})
         })
-        await User.findByIdAndUpdate(idFriend,{$pull:{confirm:idMe}},{new:true,upsert:true},(err,data)=>{
-            if (err || data == null) {
-                return res.status(400).json({message:'Utilisateur introuvable'})
-            }else{
-                return res.status(200).json({message:"Utilisateur confirmé avec succes"})
-            }
+        await User.findByIdAndUpdate(idFriend,{$pull:{confirm:idMe}},{new:true,upsert:true}).then((docs)=>{
+            return res.status(200).json({message:"Demande d'invitation annulée avec succes"})
+        }).catch((err)=>{          
+            return res.status(400).json({message:'Utilisateur introuvable'})
         })
     
     } catch (error) {
@@ -98,23 +86,19 @@ module.exports.refuseConfirmFriend = async(req,res)=>{
 
 module.exports.retirerFriend = async(req,res)=>{
 
-    const idMe =req.body.idMe;
-    const idFriend = req.params.idFriend;  
-    if (!verify.isValid(idMe) || !verify.isValid(idFriend)) {
+    const idMe =req.params.idMe;
+    const idFriend = req.body.idFriend;  
+    if (!verifyId.isValid(idMe) || !verifyId.isValid(idFriend)) {
         return res.status(500).json({err:"Id invalid"})
     }
     try {
-        await User.findByIdAndUpdate(idMe,{$pull:{friends:idFriend}},{new:true,upsert:true},(err,data)=>{
-            if (err || data == null) {
-                return res.status(400).json({message:'Utilisateur introuvable'})
-            }
+        await User.findByIdAndUpdate(idMe,{$pull:{friends:idFriend}},{new:true,upsert:true}).catch((err)=>{
+            return res.status(400).json({message:'Utilisateur introuvable'})
         })
-        await User.findByIdAndUpdate(idFriend,{$pull:{friends:idMe}},{new:true,upsert:true},(err,data)=>{
-            if (err || data == null) {
-                return res.status(400).json({message:'Utilisateur introuvable'})
-            }else{
-                return res.status(200).json({message:"Utilisateur confirmé avec succes"})
-            }
+        await User.findByIdAndUpdate(idFriend,{$pull:{friends:idMe}},{new:true,upsert:true}).then((docs)=>{
+            return res.status(200).json({message:"Utilisateur retiré des amis avec succes"})
+        }).catch((err,data)=>{
+            return res.status(400).json({message:'Utilisateur introuvable'})
         })
     
     } catch (error) {
